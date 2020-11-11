0cbd12f68ddd48f69189edd7de2be901a5e6b8d3
Dipsy Wong <ycwongal@connect.ust.hk>
2020-11-12 00:06:26 +0800

    Updates

---
+++ precache-manifest.aaa92b5a5de23bd5cc6f999ffa927aae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7196a875b193c8bbd9c7a4ab54df578",
+    "url": "/smnet/index.html"
+  },
+  {
+    "revision": "df10f74c68dadaf4fd0c",
+    "url": "/smnet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "062635b6714de188796a",
+    "url": "/smnet/static/js/2.62023a3b.chunk.js"
+  },
+  {
+    "revision": "606ffd7495665a2a2575cecd521ef7b1",
+    "url": "/smnet/static/js/2.62023a3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43287356224dce421877",
+    "url": "/smnet/static/js/3.ff0fcd55.chunk.js"
+  },
+  {
+    "revision": "df10f74c68dadaf4fd0c",
+    "url": "/smnet/static/js/main.dc7523fe.chunk.js"
+  },
+  {
+    "revision": "ac787c7cf147fd0755d8",
+    "url": "/smnet/static/js/runtime-main.bdba1306.js"
+  }
+]);
