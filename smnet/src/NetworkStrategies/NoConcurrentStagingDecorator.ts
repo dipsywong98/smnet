02797cd959df74df4f0315e63c72b63924ff21e3
Dipsy Wong <ycwongal@connect.ust.hk>
2020-12-19 21:20:19 +0800

    smnet set no concurrent staging max fail count and revaliate if staging actually promoted already

--- smnet/src/NetworkStrategies/NoConcurrentStagingDecorator.ts
+++ smnet/src/NetworkStrategies/NoConcurrentStagingDecorator.ts
@@ -3,24 +3,38 @@ import { NetworkAction, NetworkState } from '../types'
 import { NetworkBusyError } from '../Errors'
 import { NetworkStrategy } from './NetworkStrategy'
 import { logger } from '../Logger'
+import checksum from 'checksum'
 
 export class NoConcurrentStagingDecorator<State extends NetworkState, Action extends NetworkAction> extends AbstractNetworkStrategyDecorator<State, Action> {
+  private failCount = 0
+  private MAX_RESET = 10
   public async dispatch (action: Action): Promise<void> {
-    if (this.stagingState !== undefined) {
-      logger.error('cannot dispatch when there is staging state', this.stagingState)
-      throw new NetworkBusyError()
-    }
+    this.checkStagingState('cannot dispatch when there is staging state')
     // next action
     return await this.wrappedStrategy.dispatch(action)
   }
 
   public async handleDispatch (prevState: State, action: Action): Promise<State> {
-    if (this.stagingState !== undefined) {
-      logger.error('cannot handle dispatch request when there is staging state', this.stagingState)
-      throw new NetworkBusyError()
-    }
+    this.checkStagingState('cannot handle dispatch request when there is staging state')
     // next action
     return await this.wrappedStrategy.handleDispatch(prevState, action)
+  }
+
+  private checkStagingState (errorMessage: string): void {
+    if(this.failCount >= this.MAX_RESET) {
+      this.stagingState = undefined
+    }
+    if (this.stagingState !== undefined) {
+      if(checksum(JSON.stringify(this.stagingState)) === checksum(JSON.stringify(this.network.state))) {
+        this.stagingState = undefined
+      } else {
+        logger.error(errorMessage, this.stagingState)
+        this.failCount++
+        throw new NetworkBusyError()
+      }
+    } else {
+      this.failCount = 0
+    }
   }
 }
 
